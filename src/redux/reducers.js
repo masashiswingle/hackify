fe42cf306d6d39bcbf47dfbca90a8abd691c46bc
Joanne <jeffiefeifei@gmail.com>
2016-11-04 14:51:07 -0700

    [add] landing page with Redux

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,14 +1,27 @@
 // import { LOG } from './actions';
 
-function log(state = {}, action) {
+// function log(state = {}, action) {
+//   switch (action.type) {
+//     case 'LOG':
+//       return {
+//         text: action.text
+//       };
+//     default:
+//       return state;
+//   }
+// }
+//
+// export default log;
+
+function switchView(state = {}, action) {
   switch (action.type) {
-    case 'LOG':
+    case 'SWITCH_VIEW':
       return {
-        text: action.text
+        view: action.view
       };
     default:
       return state;
   }
 }
 
-export default log;
+export default switchView;
