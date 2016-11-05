614c5ef586c651a482b39494a6a801877e5a039c
Alina <alina.lobastova.8@gmail.com>
2016-11-05 15:10:50 -0700

    [remove] old annyang file

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-function mainReducer(state = {}, action) {
+const mainReducer = function (state = {}, action) {
   console.log('in reducer', action)
   switch (action.type) {
     case 'SWITCH_VIEW_TO_PLAYER':
