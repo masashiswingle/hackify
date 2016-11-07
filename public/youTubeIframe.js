15f91256c64d0532690c5029300c4f7b1075a365
Joanne <jeffiefeifei@gmail.com>
2016-11-07 11:53:30 -0800

    [testing]

--- public/youTubeIframe.js
+++ public/youTubeIframe.js
@@ -4,4 +4,4 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player = 'hello';
+var player;
