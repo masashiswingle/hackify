712b2e5987e3a532dfc0070c8687baf549cd5596
Joanne <jeffiefeifei@gmail.com>
2016-11-09 10:08:32 -0800

    [create] audio player

--- src/components/ControlBarComponent.jsx
+++ src/components/ControlBarComponent.jsx
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import $ from 'jquery';
 
 class ControlBar extends Component {
+  playOrPause() {
+    var $btn = $('.play-button');
+    if ($btn.hasClass('on')) {
+      this.play();
+    } else {
+      this.pause();
+    }
+    $('.play-button i').toggle();
+    $btn.toggleClass('on');
+  }
+
   play() {
     this.props.player.playVideo();
   }
@@ -10,12 +21,26 @@ class ControlBar extends Component {
     this.props.player.pauseVideo();
   }
 
+  stop() {
+    this.props.player.stopVideo();
+  }
+
+  next() {
+    console.log("hello next");
+    // this.props.player.nextVideo();
+  }
+
+  previous() {
+    console.log("Hello previous");
+    // this.props.player.previousVideo();
+  }
+
   mute() {
     this.props.player.isMuted() ? player.unMute() : player.mute();
   }
 
   volume() {
-    var currentVolume = $('#volume').val();
+    var currentVolume = $('#volumebar').val();
     this.props.player.setVolume(currentVolume);
   }
 
@@ -40,39 +65,47 @@ class ControlBar extends Component {
   render() {
     return(
       <div id="controlDiv">
-        <div className="progress-wrap" onClick={ this.progress.bind(this) }>
-          <div className="progress-bar"></div>
-        </div>
-        <div onClick={ this.play.bind(this) }>
-          <svg id="play" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <g>
-              <title>Play</title>
-              <polygon points="1,1 1,24 20,13"></polygon>
-            </g>
-          </svg>
-        </div>
-        <div onClick={ this.pause.bind(this) }>
-          <svg id="pause" width="25px" height="25px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <g>
-              <title>Pause</title>
-              <rect x="2" y="1" width="8px" height="23px"></rect>
-              <rect x="15" y="1" width="8px" height="23px"></rect>
-            </g>
-          </svg>
-        </div>
-        <div onClick={ this.mute.bind(this) }>
-          <svg id="mute" width="48px" height="48px" xmlns="http://www.w3.org/2000/svg" version="1.1">
-            <title>title</title>
-              <circle cx="24" cy="24" r="34">
-                <title>Mute audio</title>
-              </circle>
-            <path id="mute-audio-on" className="hidden" transform="scale(0.6), translate(17,18)" d="M38 22h-3.4c0 1.49-.31 2.87-.87 4.1l2.46 2.46C37.33 26.61 38 24.38 38 22zm-8.03.33c0-.11.03-.22.03-.33V10c0-3.32-2.69-6-6-6s-6 2.68-6 6v.37l11.97 11.96zM8.55 6L6 8.55l12.02 12.02v1.44c0 3.31 2.67 6 5.98 6 .45 0 .88-.06 1.3-.15l3.32 3.32c-1.43.66-3 1.03-4.62 1.03-5.52 0-10.6-4.2-10.6-10.2H10c0 6.83 5.44 12.47 12 13.44V42h4v-6.56c1.81-.27 3.53-.9 5.08-1.81L39.45 42 42 39.46 8.55 6z" fill="white"/>
-            <path id="mute-audio-off" transform="scale(0.6), translate(17,18)"  d="M24 28c3.31 0 5.98-2.69 5.98-6L30 10c0-3.32-2.68-6-6-6-3.31 0-6 2.68-6 6v12c0 3.31 2.69 6 6 6zm10.6-6c0 6-5.07 10.2-10.6 10.2-5.52 0-10.6-4.2-10.6-10.2H10c0 6.83 5.44 12.47 12 13.44V42h4v-6.56c6.56-.97 12-6.61 12-13.44h-3.4z"  fill="white"/>
-          </svg>
-        </div>
-        <div id="volumeDiv" onChange={ this.volume.bind(this) }>
-          <label htmlFor="volume">Volume</label>
-          <input type="range" id="volume" title="Volume" min="0" max="100" step="1"></input>
+
+        <div className='audio-player'>
+          <div className="display-panel">
+            <img src={"https://s-media-cache-ak0.pinimg.com/originals/8f/42/f8/8f42f814e0618519261b9cca4404a757.gif"} />
+          </div>
+
+          <div className="control-panel">
+            <div className="progress-wrap" onClick={ this.progress.bind(this) }>
+              <div className="progress-bar"></div>
+            </div>
+
+            <div className="audio-player-buttons">
+
+              <div className="audio-player-button" onClick={ this.previous.bind(this) }>
+                <i className="fa fa-fast-backward"></i>
+              </div>
+
+              <div className="audio-player-button play-button" onClick={ this.playOrPause.bind(this) }>
+                <i className="fa fa-play"></i>
+                <i className="fa fa-pause"></i>
+              </div>
+
+              <div className="audio-player-button" onClick={ this.stop.bind(this) }>
+                <i className="fa fa-stop"></i>
+              </div>
+
+              <div className="audio-player-button" onClick={ this.next.bind(this) }>
+                <i className="fa fa-fast-forward"></i>
+              </div>
+
+              <div className="audio-player-button mute-button" onClick={ this.mute.bind(this) }>
+                <i className="fa fa-volume-off"></i>
+              </div>
+
+              <div className="volumeDiv" onChange={ this.volume.bind(this) }>
+                <i id="volumeIcon" className="fa fa-volume-up"></i>
+                <input type="range" id="volumebar" title="Volume" min="0" max="100" step="1"></input>
+              </div>
+
+            </div>
+          </div>
         </div>
       </div>
     );
