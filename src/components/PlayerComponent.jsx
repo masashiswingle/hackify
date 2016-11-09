15761d299a203d2f1ec0088fd6c8f0ab435221ff
Jemil Ezzet <jemil.ezzet@gmail.com>
2016-11-09 12:03:48 -0800

    [add] logic for songHistory in redux store

--- src/components/PlayerComponent.jsx
+++ src/components/PlayerComponent.jsx
@@ -4,6 +4,7 @@ import ControlBar from './ControlBarComponent';
 import * as helpers from '../modules/ajax';
 import { annyangCall } from '../annyang';
 import { initiateQueue, initiateHistory, changeCurrentSong } from '../redux/actions';
+import Song from '../modules/Song';
 // import $ from 'jquery';
 
 class Player extends Component {
@@ -16,7 +17,9 @@ class Player extends Component {
   queueSong(string) {
     console.log('triggered queue');
     helpers.youTubeGetSong(string = $('#searchPlayerComp').val(), (response) => {
-      this.props.songQueue.push(response.items[0].id.videoId);
+      var queuedSong = new Song(response.items[0].id.videoId, response.items[0].snippet.title, response.items[0].snippet.thumbnails.default.url);
+      this.props.songQueue.push(queuedSong);
+      console.log(this.props);
     });
   }
 
@@ -37,8 +40,9 @@ class Player extends Component {
 
     function onPlayerStateChange(event) {
       if (event.data === 0) {
+        this.props.songHistory.unshift(this.props.currentSong);
         if (this.props.songQueue.length > 0) {
-          this.props.changeCurrentSong(this.props.songQueue[0]);
+          this.props.changeCurrentSong(this.props.songQueue[0].videoId);
           this.props.songQueue.shift();
         }
       }
@@ -75,7 +79,8 @@ const mapStateToProps = (state) => {
   return {
     view: state.view,
     currentSong: state.currentSong,
-    songQueue: state.songQueue
+    songQueue: state.songQueue,
+    songHistory: state.songHistory
   };
 };
 
