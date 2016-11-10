5db271337ebf7d1664f759d2a6fa266dcfeb10d1
Jemil Ezzet <jemil.ezzet@gmail.com>
2016-11-09 19:50:23 -0800

    [add] view for songHistory

--- src/components/LineupComponent.jsx
+++ src/components/LineupComponent.jsx
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Queue from './QueueComponent';
+import History from './HistoryComponent';
 
 class Lineup extends Component {
 
   render() {
     return (
       <div className="row">
-
-        <div className="col-md-2 col-md-offset-5">
+        <div className="col-md-5">
+          <History />
+        </div>
+        <div className="col-md-2">
           <img src={ this.props.currentSong.artwork } ></img>
         </div>
-
-        <Queue />
+        <div className="col-md-5">
+          <Queue />
+        </div>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     currentSong: state.currentSong
   };
