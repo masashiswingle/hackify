b7bdb75cc076197c9106d6ce8c309be2292977ed
Alina <alina.lobastova.8@gmail.com>
2016-11-10 18:49:01 -0800

    [add] add test for retrieving lyrics data

--- spec/server/apiQueries.js
+++ spec/server/apiQueries.js
@@ -13,8 +13,8 @@ describe('Requesting Audio Data', () => {
     done();
   });
 
-   describe('Spotify', () => {
-    it('Should respond with array of Artists', (done) => {
+   describe('Spotify Data', () => {
+    it('should respond with artis\'s id', (done) => {
       server.post('/getSongs')
       .send({
         string: 'adele hello'
@@ -22,7 +22,69 @@ describe('Requesting Audio Data', () => {
       .expect(200)
       .end((err, res) => {
         if (err) return done(err);
-        res.body.tracks.items.should.be.instanceOf(Array);
+        res.body.tracks.items[0].artists[0].id.should.be.type('string');
+        done();
+      });
+    });
+  });
+
+  describe('Top Tracks', () => {
+    it('should respond with array of songs', (done) => {
+      server.post('/artistTracks')
+      .send({
+        string: 'muse hysteria'
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.tracks.should.be.instanceOf(Array);
+        done();
+      });
+    });
+  });
+
+  describe('Artist\'s Albums', () => {
+    it('should respond with array of albums', (done) => {
+      server.post('/artistAlbums')
+      .send({
+        string: 'arctic monkeys r u mine'
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        res.body.items.should.be.instanceOf(Array);
+        done();
+      });
+    });
+  });
+
+  describe('Related Artists', () => {
+    it('should respond with array of related artists', (done) => {
+      server.post('/relatedArtists')
+      .send({
+        string: 'lady gaga poker face'
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.body.artists[0].popularity.should.be.a.Number;
+        done();
+      });
+    });
+  });
+
+    describe('Lyrics', () => {
+    it('should respond with array of related artists', (done) => {
+      server.post('/lyrics')
+      .send({
+        artist: 'radiohead',
+        track: 'karma police'
+      })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        res.should.be.a.String;
         done();
       });
     });
