e7433ad1cc581f56a35fdfbc7ca58a28599aa395
Jemil Ezzet <jemil.ezzet@gmail.com>
2016-11-12 16:17:08 -0800

    [add] functionality to picking song from queue or history

--- src/components/HistoryComponent.jsx
+++ src/components/HistoryComponent.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { changeCurrentSong, modifyHistory } from '../redux/actions';
 
 var reverseSongHistory = [null, null, null, null, null];
 
 class History extends Component {
+
+  // selectSongFromQueue(song) {
+  //   var newHistory = this.props.songHistory.filter(function(origSong) {
+  //     if (origSong.videoId !== song.videoId) {
+  //       return origSong;
+  //     }
+  //   });
+  //   console.log(newHistory);
+  //   this.props.modifyHistory(newHistory);
+  //   this.props.changeCurrentSong(song);
+  // }
 
   render() {
 
@@ -15,11 +27,12 @@ class History extends Component {
           {
             reverseSongHistory.map(function(song, index) {
               if (song) {
+                // return <img className="history available" onClick={ () => { this.selectSongFromQueue(song)} } src={ song.artwork }></img>
                 return <img className="history available" src={ song.artwork }></img>
               } else {
                 return <img className="history" src="http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=16295283"></img>
               }
-            })
+            }, this)
           }
         </div>
       );
@@ -33,9 +46,10 @@ class History extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     songHistory: state.songHistory
   };
 };
 
-export default connect(mapStateToProps)(History);
+export default connect(mapStateToProps, { changeCurrentSong: changeCurrentSong, modifyHistory: modifyHistory })(History);
