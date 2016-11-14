d113a0d9481177ec8a9f736988d41b91ab1f32cc
Joanne <jeffiefeifei@gmail.com>
2016-11-14 14:56:44 -0800

    [fix] react tabs index

--- src/components/LyricsComponent.jsx
+++ src/components/LyricsComponent.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { getLyrics } from '../modules/ajax';
 
 class Lyrics extends Component {
   constructor(props) {
     super(props);
+    this.state = { lyrics: "Searching...", lyricsDone: false }
+  }
+
+  componentDidMount() {
+    this.displayLyrics();
+  }
+
+  componentDidUpdate() {
+    this.displayLyrics();
+  }
+
+  displayLyrics() {
+    if (this.state.lyricsDone) {
+      return;
+    }
+    var that = this;
+    getLyrics(this.props.currentSong.songName, this.props.currentSong.artistName, function(data) {
+      that.setState({ lyrics: data, lyricsDone: true });
+    });
   }
 
   render() {
     return(
-      <div>{this.props.lyrics}</div>
+      <div>{this.state.lyrics}</div>
     )
   }
 }
