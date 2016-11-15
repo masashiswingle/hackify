1ecbf6759fb5f30bdcecebaf94d275167a98436b
Joanne <jeffiefeifei@gmail.com>
2016-11-15 11:51:11 -0800

    [create] landingFeature component

---
+++ src/components/LandingFeatureComponent.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+
+class LandingFeature extends Component {
+
+  render() {
+    return(
+
+      <div className="container-fluid">
+
+        <br></br><br></br>
+
+        <h1 id="getStart">How to get started?</h1>
+
+        <br></br><br></br>
+
+        <div className="row">
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <img className="landingpic" id="interaction" src={'/assets/interaction.jpg'}/>
+          </div>
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <h3>real-time</h3>
+            <h2>interaction</h2>
+            <h4>_________</h4>
+            <h4>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah</h4>
+            <button className="learnmore" type="button">learn more</button>
+          </div>
+        </div>
+
+        <br></br><br></br>
+        <br></br><br></br>
+
+        <div className="row">
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <h3>variety of</h3>
+            <h2>commands</h2>
+            <h4>_________</h4>
+            <h4>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah</h4>
+            <button className="learnmore" type="button">learn more</button>
+          </div>
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <img className="landingpic" id="sound" src={'/assets/sound.jpg'}/>
+          </div>
+        </div>
+
+        <br></br><br></br>
+        <br></br><br></br>
+
+        <div className="row">
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <img className="landingpic" id="info" src={'/assets/info.jpg'}/>
+          </div>
+          <div className="col-sm-6 col-md-6 col-lg-6">
+            <h3>ask Bear for any</h3>
+            <h2>information</h2>
+            <h4>_________</h4>
+            <h4>Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah</h4>
+            <button className="learnmore" type="button">learn more</button>
+          </div>
+        </div>
+
+      </div>
+    )
+  }
+}
+
+
+export default LandingFeature;
