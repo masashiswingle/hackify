e1df23932e8198fb2e7a9e631e8698034fe0deb9
Joanne <jeffiefeifei@gmail.com>
2016-11-16 14:55:15 -0800

    [render] all albums of artist

--- src/components/AlbumComponent.jsx
+++ src/components/AlbumComponent.jsx
@@ -5,7 +5,7 @@ import { artistAlbums } from '../modules/ajax';
 class Album extends Component {
   constructor(props) {
     super(props);
-    this.state = { album: "Searching...", videoId: this.props.currentSong.videoId }
+    this.state = { album: [], videoId: this.props.currentSong.videoId }
   }
 
   componentDidMount() {
@@ -22,13 +22,29 @@ class Album extends Component {
     var that = this;
     artistAlbums(this.props.currentSong.artistName, function(data) {
       console.log("here is my data", data);
-      that.setState({ album: data, videoId: that.props.currentSong.videoId });
+      that.setState({ album: data.items, videoId: that.props.currentSong.videoId });
+      console.log("here is the arr", that.state.album);
     });
   }
 
   render() {
+    console.log("please show something", this.state.album);
+    if(this.state.album.length === 0) {
+      return (
+        <div>Loading...</div>
+      )
+    }
     return(
-      <div id ='album'></div>
+      <div>
+        {this.state.album.map(function(album, index){
+          return (
+            <div key={index}>
+              <img src={album.images[1].url}></img>
+              <div>{album.name}</div>
+            </div>
+          );
+        }, this)}
+      </div>
     );
   }
 }
