3517d19561f45779ee5fe1c45ea360b451ad5acf
Joanne <jeffiefeifei@gmail.com>
2016-11-17 22:55:22 -0800

    [create] Most popular component

---
+++ src/components/MostPopularComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MostPopular extends Component {
+
+  render() {
+    return(
+      <div>
+        hihihi
+      </div>
+    )
+  }
+}
+
+
+export default MostPopular;
