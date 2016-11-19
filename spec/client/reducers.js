da875969712f4d3a5ff7bca3fa6b5332fe553872
Alina <alina.lobastova.8@gmail.com>
2016-11-18 19:46:49 -0800

    [add]write client tests up to remove_from_history reducer

--- spec/client/reducers.js
+++ spec/client/reducers.js
@@ -113,7 +113,6 @@ describe('Reducers-', () => {
       artistName: "Arctic Monkeys",
       artistId: "7Ln80lUS6He07XvHI8qqHH",
       countries: ["US"]
-      
     };
     it('mainReducer: Should add new song to queue', () => {
       let state =  mainReducer({}, oldData);
@@ -156,8 +155,8 @@ describe('Reducers-', () => {
       countries: ["US"]
     };
     let actionDequeue = {
-      type: 'DEQUEUE_SONG',
-    }
+      type: 'DEQUEUE_SONG'
+    };
     it('mainReducer: Should set current song when dequeued', () => {
       let state =  mainReducer({}, oldData);
       mainReducer({}, oldData).currentSong.artistName.should.equal('Adele');
@@ -167,6 +166,202 @@ describe('Reducers-', () => {
 
       let dequeued = mainReducer(initiated, newData);
       mainReducer(dequeued, actionDequeue).currentSong.artistName.should.equal('Arctic Monkeys');
+    });
+  });
+
+  describe('ADD_TO_HISTORY', () => {
+    let oldData = {
+      type: 'SWITCH_VIEW_TO_PLAYER',
+      view: 'player',
+      currentSong: {
+        videoId: 'YQHsXMglC9A',
+        title: 'Adele - Hello',
+        artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+        albumName: "Adele",
+        songName: 'Hello',
+        artistName: 'Adele',
+        artistId: '4dpARuHxo51G3z768sgnrY',
+        countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX","NI","PA","PE","PY","SV","US","UY"]
+      }
+    };
+    let queue = {
+      type: 'INITIATE_QUEUE',
+      songQueue: [],
+      songHistory: []
+    };
+    let actionHistory = {
+      type: 'ADD_TO_HISTORY',
+      song: oldData.currentSong
+    };
+    it('mainReducer: Should add current song to history when played', () => {
+      let state =  mainReducer({}, oldData);
+      mainReducer({}, oldData).currentSong.artistName.should.equal('Adele');
+
+      let initiated = mainReducer(state, queue);
+      mainReducer(initiated, actionHistory).songHistory[0].artistName.should.equal('Adele');
+      mainReducer(initiated, actionHistory).currentSong.artistName.should.not.be.ok;
+    });
+  });
+
+  describe('MODIFY_QUEUE', () => {
+    let oldData = {
+      type: 'SWITCH_VIEW_TO_PLAYER',
+      view: 'player',
+      currentSong: {
+        videoId: 'YQHsXMglC9A',
+        title: 'Adele - Hello',
+        artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+        albumName: "Adele",
+        songName: 'Hello',
+        artistName: 'Adele',
+        artistId: '4dpARuHxo51G3z768sgnrY',
+        countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX","NI","PA","PE","PY","SV","US","UY"]
+      }
+    };
+    let queue = {
+      type: 'INITIATE_QUEUE',
+      songQueue: [],
+      songHistory: []
+    };
+    let newData = {
+      type: 'ADD_TO_QUEUE',
+      view: 'player',
+      videoId: "Jn6-TItCazo",
+      title: "Arctic Monkeys - Arabella (Official Audio)",
+      artwork: "https://i.scdn.co/image/10c7b4705032bc99da2d32ec2751ab3fdd64ca5f",
+      albumName: "AM",
+      songName: "Arabella",
+      artistName: "Arctic Monkeys",
+      artistId: "7Ln80lUS6He07XvHI8qqHH",
+      countries: ["US"]
+    };
+    let newQueue = {
+      type: 'ADD_TO_QUEUE',
+      view: 'player',
+      videoId: 'YQHsXMglC9A',
+      title: 'Adele - Hello',
+      artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+      albumName: "Adele",
+      songName: 'Hello',
+      artistName: 'Adele',
+      artistId: '4dpARuHxo51G3z768sgnrY',
+      countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX"]
+    };
+
+    let modifiedQueue = {
+      type: 'MODIFY_QUEUE',
+      newQueue: [{
+        type: 'ADD_TO_QUEUE',
+        view: 'player',
+        videoId: 'YQHsXMglC9A',
+        title: 'Adele - Hello',
+        artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+        albumName: "Adele",
+        songName: 'Hello',
+        artistName: 'Adele',
+        artistId: '4dpARuHxo51G3z768sgnrY',
+        countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX"]
+      }]
+    };
+
+
+    it('mainReducer: Should remove chosen song from queue', () => {
+      let state =  mainReducer({}, oldData);
+      mainReducer({}, oldData).currentSong.artistName.should.equal('Adele');
+
+      let initiated = mainReducer(state, queue);
+      mainReducer(initiated, newData).songQueue[0].artistName.should.equal('Arctic Monkeys');
+
+      let newQueueCreated = mainReducer(initiated, newData);
+      mainReducer(newQueueCreated, newQueue).songQueue[1].artistName.should.equal('Adele');
+
+      let modified = mainReducer(newQueueCreated, newQueue);
+      mainReducer(modified, modifiedQueue).songQueue[0].artistName.should.not.equal('Arctic Monkeys');
+    });
+  });
+
+  describe('MODIFY_HISTORY', () => {
+    let oldData = {
+      type: 'SWITCH_VIEW_TO_PLAYER',
+      view: 'player',
+      currentSong: {
+        videoId: 'YQHsXMglC9A',
+        title: 'Adele - Hello',
+        artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+        albumName: "Adele",
+        songName: 'Hello',
+        artistName: 'Adele',
+        artistId: '4dpARuHxo51G3z768sgnrY',
+        countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX","NI","PA","PE","PY","SV","US","UY"]
+      }
+    };
+    let queue = {
+      type: 'INITIATE_QUEUE',
+      songQueue: [],
+      songHistory: []
+    };
+    let actionHistory = {
+      type: 'ADD_TO_HISTORY',
+      song: oldData.currentSong
+    };
+
+    let modifiedHistory = {
+      type: 'MODIFY_HISTORY',
+      newHistory: []
+    };
+
+    it('mainReducer: Should remove chosen song from history', () => {
+      let state =  mainReducer({}, oldData);
+      mainReducer({}, oldData).currentSong.artistName.should.equal('Adele');
+
+      let initiated = mainReducer(state, queue);
+      mainReducer(initiated, actionHistory).songHistory[0].artistName.should.equal('Adele');
+      mainReducer(initiated, actionHistory).currentSong.artistName.should.not.be.ok;
+
+      let modifier = mainReducer(initiated, actionHistory);
+      mainReducer(modifier, modifiedHistory).songHistory.should.be.empty;
+    });
+  });
+
+  describe('PLAY_PREVIOUS', () => {
+    let oldData = {
+      type: 'SWITCH_VIEW_TO_PLAYER',
+      view: 'player',
+      currentSong: {
+        videoId: 'YQHsXMglC9A',
+        title: 'Adele - Hello',
+        artwork: 'https://i.scdn.co/image/f71517e8919892273de8d8677e42cdcf1b976aa7',
+        albumName: "Adele",
+        songName: 'Hello',
+        artistName: 'Adele',
+        artistId: '4dpARuHxo51G3z768sgnrY',
+        countries: ["AR","BO","BR","CL","CO","CR","DO","EC","GT","HN","MX","NI","PA","PE","PY","SV","US","UY"]
+      }
+    };
+    let queue = {
+      type: 'INITIATE_QUEUE',
+      songQueue: [],
+      songHistory: []
+    };
+    let actionHistory = {
+      type: 'ADD_TO_HISTORY',
+      song: oldData.currentSong
+    };
+    let action = {
+      type: 'PLAY_PREVIOUS'
+    }
+    it('mainReducer: Should play previous song', () => {
+      let state =  mainReducer({}, oldData);
+      mainReducer({}, oldData).currentSong.artistName.should.equal('Adele');
+
+      let initiated = mainReducer(state, queue);
+      mainReducer(initiated, actionHistory).songHistory[0].artistName.should.equal('Adele');
+      mainReducer(initiated, actionHistory).currentSong.artistName.should.not.be.ok;
+
+      let addedToHistory = mainReducer(initiated, actionHistory);
+      mainReducer(addedToHistory, action).currentSong.artistName.should.equal('Adele');
+      mainReducer(addedToHistory, action).songHistory.should.be.empty;
+      mainReducer(addedToHistory, action).songQueue.should.be.empty;
     });
   });
 
