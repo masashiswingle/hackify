617299d79d7c320c592daca430c8688f25b44e9e
Masashi <masashiswingle@hotmail.com>
2016-11-19 14:40:15 -0800

    [ADD] Desc to Landing home page

--- src/components/LandingFeatureComponent.jsx
+++ src/components/LandingFeatureComponent.jsx
@@ -34,9 +34,9 @@ class LandingFeature extends Component {
             <h3>variety of</h3>
             <h2>commands</h2>
             <h4 className='text-mainunderline'>_________</h4>
-            <h4 className='txt'> soundBear offers an additonal number of additional commands on top of playing a song.  Any button immaginable on a music player
-            can be said effortlessly in voice. For example, adding a song to the song queue or pausing a selected song can be voiced by just "Add to Queue" 
-            Song by Artist and "Stop", respectively. 
+            <h4 className='txt'> soundBear offers a number of additional commands on top of playing a song.  Any button imaginable on a music player
+            can be said effortlessly in voice for soundBear to understand. For example, adding a song to the song queue or pausing a selected song can be voiced by just "Add to Queue Song by Artist" 
+             and "Stop", respectively. 
 </h4>
           </div>
           <div className="col-sm-6 col-md-6 col-lg-6">
@@ -55,7 +55,7 @@ class LandingFeature extends Component {
             <h3>ask Bear for any</h3>
             <h2>information</h2>
             <h4 className='text-mainunderline'>_________</h4>
-            <h4 className = 'txt'>For every song that plays, there will also be useful information regarding the Song and Artist presented in the tabs below. 
+            <h4 className = 'txt'>For every song that plays, there will also be useful information regarding the Song and Artist presented in the tabs below the music player. 
             The tabs from left to right represent Lyrics, Most Popular Songs on our App, Top Songs of that Artist, Artist Albums and Available Markets, and 
             Related Artists</h4>
           </div>
