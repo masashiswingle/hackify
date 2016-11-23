5413b02ff9e949a9cfc87efaa3d7e150ae3d16e7
Joanne <jeffiefeifei@gmail.com>
2016-11-22 16:27:52 -0800

    [fix] bug for react tab, prevent from jumping to top

--- src/components/NavComponent.jsx
+++ src/components/NavComponent.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import ScrollArea from 'react-scrollbar';
 import Lyrics from './LyricsComponent';
 import MostPopular from './MostPopularComponent';
 import Tree from './TreeComponent';
@@ -14,35 +15,35 @@ class Nav extends Component {
 
   render() {
     return(
+      <div style={{height: "800px"}}>
+        <Tabs className="navTabs" selectedIndex={this.index}>
 
-      <Tabs className="navTabs" selectedIndex={this.index}>
 
-          <TabList className="navlist">
-            <Tab title="Lyrics" data-toggle="popover" data-trigger="hover">
-              <img className="nav" id="playlist" src={'/assets/playlist.png'} />
-            </Tab>
-            <Tab title="soundBear Top" data-toggle="popover" data-trigger="hover">
-              <img className="nav" id="worldMap" src={'/assets/heart.png'} />
-            </Tab>
-            <Tab title="Artist's Top" data-toggle="popover" data-trigger="hover">
-              <img className="nav" id="audacity" src={'/assets/audacity.png'} />
-            </Tab>
-            <Tab title="Albums" data-toggle="popover" data-trigger="hover">
-              <img className="nav" id="uncheck" src={'/assets/uncheck.png'} />
-            </Tab>
-            <Tab title="Related Artists" data-toggle="popover" data-trigger="hover">
-              <img className="nav" id="tournament" src={'/assets/tournament.png'} />
-            </Tab>
-          </TabList>
+            <TabList className="navlist">
+              <Tab title="Lyrics" data-toggle="popover" data-trigger="hover">
+                <img className="nav" id="playlist" src={'/assets/playlist.png'} />
+              </Tab>
+              <Tab title="soundBear Top" data-toggle="popover" data-trigger="hover">
+                <img className="nav" id="worldMap" src={'/assets/heart.png'} />
+              </Tab>
+              <Tab title="Artist's Top" data-toggle="popover" data-trigger="hover">
+                <img className="nav" id="audacity" src={'/assets/audacity.png'} />
+              </Tab>
+              <Tab title="Albums" data-toggle="popover" data-trigger="hover">
+                <img className="nav" id="uncheck" src={'/assets/uncheck.png'} />
+              </Tab>
+              <Tab title="Related Artists" data-toggle="popover" data-trigger="hover">
+                <img className="nav" id="tournament" src={'/assets/tournament.png'} />
+              </Tab>
+            </TabList>
 
-          <TabPanel ><Lyrics/></TabPanel>
-          <TabPanel><MostPopular/></TabPanel>
-          <TabPanel><Track/></TabPanel>
-          <TabPanel><Album/></TabPanel>
-          <TabPanel><Tree/></TabPanel>
-
-      </Tabs>
-
+            <TabPanel ><Lyrics/></TabPanel>
+            <TabPanel><MostPopular/></TabPanel>
+            <TabPanel><Track/></TabPanel>
+            <TabPanel><Album/></TabPanel>
+            <TabPanel><Tree/></TabPanel>
+        </Tabs>
+      </div>
     );
   }
 }
